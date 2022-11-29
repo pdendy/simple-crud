e9a0e71656cc6396c62939f7a00a95719bce02ae
pdendy <68658567+dendypat@users.noreply.github.com>
2022-11-29 16:43:56 -0500

    "add inputs"

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,34 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input type="number" />
+        <label>Country:</label>
+        <input type="text" />
+        <label>Position:</label>
+        <input type="text" />
+        <label>Wage: (year):</label>
+        <input type="number" />
+        <button>Add Employee</button>
+      </div>
+    </div>
   );
 }
 
