8f633b81b8e5225b0dde58e85baaedac0554ba86
pdendy <68658567+dendypat@users.noreply.github.com>
2022-12-02 11:29:43 -0500

    "add axios and cors"

--- server/index.js
+++ server/index.js
@@ -1,33 +1,38 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'local',
-    password: 'root',
-    database: 'employeesystem'
-})
+  user: "root",
+  host: "local",
+  password: "root",
+  database: "employeesystem",
+});
 
-app.post('/create', (req, res) => {
-    const name = req.body.name;
-    const age = req.body.age;
-    const country = req.body.country;
-    const position = req.body.position;
-    const wage = req.body.wage;
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
 
-    db.query("INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)", 
-    [name, age, country, position, wage], 
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
     (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send("Values inserted");
-        }
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values inserted");
+      }
     }
-    );
+  );
 });
 
 app.listen(3001, () => {
-    console.log('Yah, your server is running on port 3001');
+  console.log("Yah, your server is running on port 3001");
 });
